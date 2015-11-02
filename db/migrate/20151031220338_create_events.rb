d822d9cea526c69f8bd48359f67626c5e7982800
Iceberg20 <italoberg@yahoo.com.br>
2015-11-01 21:05:18 -0300

    init

--- db/migrate/20151031220338_create_events.rb
+++ db/migrate/20151031220338_create_events.rb
@@ -2,7 +2,7 @@ class CreateEvents < ActiveRecord::Migration
   def change
     create_table :events do |t|
       t.string :titulo
-      t.tetx :descricao
+      t.text :descricao
       t.string :endereco
       t.datetime :data
       t.string :imagem
