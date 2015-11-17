4999811663039c4f812f79eef9633c30f2ebcfa1
Iceberg20 <italoberg@yahoo.com.br>
2015-11-17 20:46:17 -0300

    editing user, routes and controller omniauth methods

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks' }
-
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   resources :events
+  
+  devise_scope :user do
+    delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+  end
 
   root to: 'events#index'
   # The priority is based upon order of creation: first created -> highest priority.
