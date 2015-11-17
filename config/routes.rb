1ed184972ef55151e3626dbba53b207f44443ac8
Iceberg20 <italoberg@yahoo.com.br>
2015-11-17 15:59:46 -0300

    adding route for callback

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   resources :events
 
-  match "/auth/:provider/callback" => "events#index"
+  get "/auth/:provider/callback" => "events#index"
 
   root to: 'events#index'
   # The priority is based upon order of creation: first created -> highest priority.
