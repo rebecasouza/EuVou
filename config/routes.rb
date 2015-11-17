2384ff85fb90151ffb3529912f8551f266fb52bd
Iceberg20 <italoberg@yahoo.com.br>
2015-11-17 19:18:58 -0300

    removing get route for callback

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   resources :events
 
-  get "/auth/:provider/callback" => "events#index"
-
   root to: 'events#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
