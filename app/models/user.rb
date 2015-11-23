f9575783a8869200c7a739b0f3c740a280d35b35
rebecasouza <rebecasouza1985@gmail.com>
2015-11-23 09:13:47 -0300

    add omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,17 +5,23 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   has_many :events
   
+  def self.sign_in_from_omniauth(auth)
+    find_or_create_by(provider: auth['provider'], uid: auth['uid']) 
+  end
   
-  def self.from_omniauth(auth)
-    where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
-      user.provider = auth.provider
-      user.uid = auth.uid
-      user.name = auth.info.name
-      user.image = auth.info.image
-      user.oauth_token = auth.credentials.token
-      user.oauth_expires_at = Time.at(auth.credentials.expires_at)
+  # gets all the info from the use's facebook account
+  def self.create_user_from_omniauth(auth)
+    create(
+      user.provider = auth['provider'],
+      user.uid = auth['uid'],
+      user.name = auth['info']['name']
+      user.email = auth['info']['email']
+      user.image = auth['info']['image']
+      user.oauth_token = auth['credentials']['token']
+      user.oauth_expires_at = Time.at(auth['credentials']['expires_at'])
       user.save!
-    end
+      )
   end
   
 end
+
