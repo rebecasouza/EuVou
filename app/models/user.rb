8a0f7bf8b68bfe85badd9e52768cb4faa65835fb
rebecasouza <rebecasouza1985@gmail.com>
2015-11-23 09:41:55 -0300

    adding omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,7 +20,6 @@ class User < ActiveRecord::Base
       user.oauth_token = auth['credentials']['token']
       user.oauth_expires_at = Time.at(auth['credentials']['expires_at'])
       user.save!
-      )
   end
   
 end
