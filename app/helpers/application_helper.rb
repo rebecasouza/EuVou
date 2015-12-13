90a41066a69f065630903dd83c7d15a9a8b7508d
rebecasouza <rebeca_souza85@hotmail.com>
2015-12-13 18:01:19 -0300

    adding helper methods for email verification, providers urls and user roles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,20 @@
 module ApplicationHelper
+	def user_email(event)
+		event.user.present? ? event.user.email : "an anonymous user"
+  end
+
+  def user_roles(user)
+    user.roles.map(&:name).join(', ').titleize
+  end
+
+  def link_to_provider(user)
+    provider = user.identities.map(&:provider).join(', ')
+    provider = case provider
+    when 'facebook' then link_to( 'Facebook', "http://www.facebook.com" )
+    when "twitter" then link_to( 'Twitter', "http://twitter.com" )
+		when "google_oauth2" then link_to( 'Google', "http://google.com" )
+		when 'forusquare'    then link_to( 'Foursquare', "http://foursquare.com")
+    end
+
+  end
 end
