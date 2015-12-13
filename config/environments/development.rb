7d72f76d0197fc46f57d05bbd8b0c01c7021e5a9
rebecasouza <rebeca_souza85@hotmail.com>
2015-12-13 18:03:21 -0300

    changing mail delivery settings to letter opener in development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,5 @@
 Rails.application.configure do
-  #Devise configuration
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
@@ -40,4 +39,9 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+	
+	#Devise configuration
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
