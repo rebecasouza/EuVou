e88dd237027630455887ac18972891d7f9a29801
rebecasouza <rebeca_souza85@hotmail.com>
2015-12-13 17:54:14 -0300

    changing signup verification methods in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,10 +21,11 @@ class ApplicationController < ActionController::Base
     end
   end
 
+=begin
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
-
+=end
   protected
 
   def configure_permitted_parameters
