8711a6fe5c652751a3f7ae52a99aa81c22dbf58e
rebecasouza <rebeca_souza85@hotmail.com>
2015-12-20 17:55:45 -0300

    adding smtp configurations to production

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,16 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+config.action_mailer.default_url_options = { :host => 'https://euvou-teste.herokuapp.com' }
+config.action_mailer.raise_delivery_errors = true
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.smtp_settings = {
+    :enable_starttls_auto => true,
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => "gmail.com",
+    :authentication => :login,
+		:user_name => "euvou.teste@gmail.com",
+    :password => "#EuVou.com#"
+}
